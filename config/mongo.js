258ec36b2979748b172f911db6848640603dfcbd
arshadvkb <arshadvkb118@gmail.com>
2025-09-26 22:42:11 +0530

    feat(dotenv): add dotenv version 17.2.2 with enhanced security and TypeScript support
    - Introduced SECURITY.md for reporting vulnerabilities.
    - Added TypeScript definitions in config.d.ts and main.d.ts.
    - Implemented config.js for loading environment variables with options.
    - Created cli-options.js and env-options.js for handling command line and environment variable options.
    - Enhanced main.js with new features for parsing and populating environment variables, including support for encrypted .env.vault files.
    - Updated package.json to include new scripts and dependencies.
    - Updated package-lock.json to reflect the addition of dotenv.

--- config/mongo.js
+++ config/mongo.js
@@ -1,8 +1,7 @@
-import mongoose from 'mongoose';
-
+import mongoose from 'mongoose'
 // Replace <db_password> with your actual MongoDB password
 export const db=async ()=>{
-await mongoose.connect('mongodb+srv://arshadvkb969_db_user:arshadvkb@cluster0.lnl0iml.mongodb.net/admin').then(() => {
+await mongoose.connect(`${process.env.MONGO_DB_URI}/admin`).then(() => {
     console.log('Connected to MongoDB');
 }).catch((err) => {
     console.error('Error connecting to MongoDB', err);
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express=require('express');
 const { db } = require('./config/mongo');
-const app=express();        
+const app=express();    
+const dotenv=require('dotenv');    
 const port=8000;
 
+app.use(express.json());
+dotenv.config();
 app.get('/',(req,res)=>{
     res.send('Hello World!');
 }); 
