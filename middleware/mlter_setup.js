76775916d57c3628ca03ec5bf31a974cec53530b
arshadvkb <arshadvkb118@gmail.com>
2025-10-07 16:50:42 +0530

    updated muslter setup

--- middleware/mlter_setup.js
+++ middleware/mlter_setup.js
@@ -2,22 +2,12 @@ import multer from "multer";
 
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/temp"); // Create this folder if needed
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
+  filename: function (req,file,cb) {
+    cb(null, file.originalname)
+   }
+  
 });
-const upload = multer({ 
-  storage: storage,
-  limits: { fileSize: 10 * 1024 * 1024 }, // 10MB limit; adjust for videos
-  fileFilter: (req, file, cb) => {
-    if (file.mimetype.startsWith('image/') || file.mimetype.startsWith('video/')) {
-      cb(null, true);
-    } else {
-      cb(new Error('Only images and videos allowed'), false);
-    }
-  }
-});
+
+const upload = multer({storage: storage});
+
 export default upload;
