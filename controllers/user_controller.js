38b8b5874829b3708638d3349a7c285716bd117d
arshadvkb <arshadvkb118@gmail.com>
2025-10-12 16:18:58 +0530

    addded view rented book function for users

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -30,42 +30,45 @@ const rentBook = async (req, res) => {
   }
 };
 
-
-
-
-
-
-
-const return_book=async(req,res)=>{
-
+const return_book = async (req, res) => {
   try {
-    const {id}=req.params
-    console.log("id="+id);
-    const rental=await rentalModel.findById(id)
-    if (!rental){
-      return res.json({success:false,message:"No rental found"}) 
+    const { id } = req.params;
+    console.log("id=" + id);
+    const rental = await rentalModel.findById(id);
+    if (!rental) {
+      return res.json({ success: false, message: "No rental found" });
     }
-    if(rental.status=== "returned"){
-        return res.json({ success: false, message: "Book already returned" }); 
+    if (rental.status === "returned") {
+      return res.json({ success: false, message: "Book already returned" });
     }
     rental.return_date = new Date();
     rental.status = "returned";
-    await rental.save()
+    await rental.save();
 
-    rental.book.available_count+=1
-    await rental.book.save()
-    
+    rental.book.available_count += 1;
+    await rental.book.save();
   } catch (error) {
     return res.json({ success: false, message: error.message });
   }
-}
+};
 
+const view_rented_book = async (req, res) => {
+  try {
+    const rentals = await rentalModel
+      .find({ user: req.params.id })
+      .sort({ rental_date: -1 });
 
+    const activeRentals = rentals.filter((r) => r.status === "active");
 
+    res.json({
+      success: true,
+      message: "rental details",
+      allRentals: rentals,
+      activeRentals,
+    });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
 
-
-
-
-
-
-export { rentBook , return_book};
+export { rentBook, return_book, view_rented_book };
--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { rentBook, return_book } from "../controllers/user_controller.js";
+import { rentBook, return_book, view_rented_book } from "../controllers/user_controller.js";
 
 const userRouter = express.Router();
 
 userRouter.put("/rent-book", rentBook);
 userRouter.put("/return-book/:id", return_book);
+userRouter.get("/view-rented-book/:id",view_rented_book);
 
 export default userRouter;
